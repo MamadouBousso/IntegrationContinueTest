22db7c8a6a7a488aa0aed17f09e4896ac0f05f0e
MamadouBousso <mbousso@univ-thies.sn>
2020-03-28 21:50:42 +0000

    Add addition and soustraction functions

---
+++ calculator.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Mar 28 21:34:17 2020
+
+@author: test
+"""
+
+"""
+Calculator library containing basic math operations.
+"""
+
+def add(first_term, second_term):
+    return first_term + second_term
+
+def subtract(first_term, second_term):
+    return first_term - second_term
